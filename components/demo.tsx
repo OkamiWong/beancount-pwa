fd87c6dadac650428ede3a5df524377bc84dedb4
Okami Wong <wtfimba@live.com>
2022-11-15 22:29:15 +0800

    FS test

--- components/demo.tsx
+++ components/demo.tsx
@@ -3,9 +3,9 @@ import { Button } from '@mantine/core'
 
 export function Demo() {
   const onButtonClick = async () => {
-    const root = await navigator.storage.getDirectory()
-    console.log(root.name)
-    const fileHandle = await root.getFileHandle('Untitled.txt', { create: true })
+    const rootDirectoryHandle = await window.showDirectoryPicker()
+    console.log(rootDirectoryHandle.name)
+    const fileHandle = await rootDirectoryHandle.getFileHandle('Untitled.txt', { create: true })
     const file = await fileHandle.getFile()
     console.log(await file.text())
   }
